6d1735691e066d0cdfd97966e33cc3738c9d4664
Alicia <alicia.manzano@hotmail.es>
2019-05-12 20:43:41 +0200

    Page data working

--- src/pages/data/data.page.ts
+++ src/pages/data/data.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-data',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.page.scss'],
 })
 export class DataPage implements OnInit {
-
-  constructor() { }
+  lenguaje: string = "";
+  formulario: string = "";
+  constructor(public menuCtrl: MenuController,
+    public navCtrl: NavController, private cookieService: CookieService) { }
 
   ngOnInit() {
+    this.menuCtrl.enable(false);
+  }
+
+  siguiente() {
+
+    this.navCtrl.navigateRoot('/data2');
+  }
+
+  selected1() {
+    if (this.lenguaje === "php")
+      this.cookieService.set('lenguaje', "php");
+
+    if (this.lenguaje === "javaScript")
+      this.cookieService.set('lenguaje', "javaScript");
+
+  }
+
+  selected2() {
+    if (this.formulario === "example")
+      this.cookieService.set('formulario', "example");
+
+    if (this.formulario === "mine")
+      this.cookieService.set('formulario', "mine");
+
   }
 
 }
