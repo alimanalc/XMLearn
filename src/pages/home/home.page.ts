2454513ebf9d3d681012132250b72a1c57dedb5a
Alicia <alicia.manzano@hotmail.es>
2019-05-12 18:30:47 +0200

    Home create

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(public menuCtrl: MenuController,
+    public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+    this.menuCtrl.enable(false);
+  }
+
+  siguiente() {
+
+    this.navCtrl.navigateRoot('/list');
+  }
 }
